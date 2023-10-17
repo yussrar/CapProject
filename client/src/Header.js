d5e458236224b5576b1ce0f915c9fcb045c9c9e6
Yusra Jamal <yusra>
2023-10-17 01:25:14 -0400

    ok

--- client/src/Header.js
+++ client/src/Header.js
@@ -49,7 +49,7 @@ const Header = () => {
           Hello, {user.name}
           {isDropdownOpen && (
             <ul className="dropdown-menu">
-              <li><Link to="http://localhost:3000/api/profile">My Profile</Link></li>
+              <li><Link to="https://cap-project-server.vercel.app/api/profile">My Profile</Link></li>
               <li><Link to="/wishlist">Wish List</Link></li>
               <li><Link to="/logout">Logout</Link></li>
             </ul>
