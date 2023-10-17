0014ced99799044d571b00fcf3d50a6818ecf8ba
Yusra Jamal <yusra>
2023-10-17 00:48:45 -0400

    done

--- client/src/Login.js
+++ client/src/Login.js
@@ -39,7 +39,6 @@ const Login = () => {
                     'Access-Control-Allow-Methods': 'GET, OPTIONS, PATCH, DELETE, POST, PUT',
                     'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
                 },
-                credentials: 'include',
                 body: JSON.stringify(formData),
             });
 
